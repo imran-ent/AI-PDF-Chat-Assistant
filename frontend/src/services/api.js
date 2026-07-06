33c20c3f90dfabfe95741cff44d76b39ee0655e6
Mohammed imran <imranambassadorofislam@gmail.com>
2026-07-06 17:56:15 +0530

    Connect frontend to deployed backend-3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://ai-pdf-chat-assistant-1.onrender.com",
-    headers: {
-        "Content-Type": "application/json",
-    },
 });
 
 export default api;
